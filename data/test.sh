4a7aee6038be00b80d8aec4b4fbbf2876fb5d071
Ratchet W <33121971+whalelahw@users.noreply.github.com>
2020-01-06 03:38:56 +0800

    update test script

--- data/test.sh
+++ data/test.sh
@@ -2,6 +2,7 @@
 
 inputDir="./input/"
 outputDir="./output/"
+solDir="./solution/"
 cnt=0
 for file in $(ls $inputDir)
 do
@@ -9,14 +10,17 @@ do
     filename="${file%.*}"
     input="$inputDir$file"
     output="$outputDir$filename.out"
+    sol="$solDir$filename.out"
     
     n=$(head -n1 $input | awk '{print $1;}')
     m=$(head -n1 $input | awk '{print $2;}')
-    echo "Starting Test #$cnt: $filename, $n x $m"
+    opt=$(sed '2q;d' $sol | awk '{print $1;}')
+    echo "Test #$cnt: $filename, $n x $m"
+    echo "opt_sol = $opt"
     
     start=$SECONDS
     ./LinearSolver.exe < $input
     duration=$(( SECONDS - start ))
-    echo "Test #$cnt: time elapsed = $duration s"
+    echo "Test #$cnt finished in $duration s"
     echo
 done
